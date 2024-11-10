58bb02c9895a98b16d04e71b1a85a5600ccf6777
dePaula06 <p.depaula06.sp@gmail.com>
2024-11-09 21:50:05 -0300

    chore(App): Addition of the Game and score pages, with their respective routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Init from './pages/init/init';
 import Autora from './pages/autora/autora';
 import Obra from './pages/obra/obra';
+import Game from './pages/game/game';
+import ScorePage from './pages/score/score';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Init />} />
         <Route path="/biografia" element={<Autora />} />
         <Route path="/obra" element={<Obra />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/score" element={<ScorePage />} />
       </Routes>
     </Router>
   );
